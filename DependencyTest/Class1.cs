e446a975acc002d27cdfba0f413e71dc8cb554fe
mike <michael@digitalreach.online>
2019-02-02 13:50:36 +0000

    add reference

--- DependencyTest/Class1.cs
+++ DependencyTest/Class1.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using RestSharp;
 
 namespace DependencyTest
 {
     public class Class1
     {
+        public Class1()
+        {
+            var test = new RestClient();
+        }
     }
 }
